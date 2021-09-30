f0076f2f245b7541070f25ad6f7cfd2a2bf05125
Florentin <dumitracheflorentincristian@gmail.com>
2021-09-30 23:59:21 +0300

    edited gallery and fixed some issues for ASMI section

--- src/pages/SectionPage.js
+++ src/pages/SectionPage.js
@@ -9,6 +9,25 @@ import data from "../data.json";
 import PageToTop from "../components/PageToTop";
 import FooterComponent from "../components/FooterComponent";
 
+// Import Images
+import apostuAlexandru from "../assets/sectiune/echipa/ApostuAlexandru.jpg";
+import ApostuMihaiAdrian from "../assets/sectiune/echipa/ApostuMihaiAdrian.jpg";
+import BadescuMadalina from "../assets/sectiune/echipa/BadescuMadalina.jpg";
+import BuzamatIonut from "../assets/sectiune/echipa/BuzamatIonut.jpg";
+import CordunDiana from "../assets/sectiune/echipa/CordunDiana.jpg";
+import DragomirAlexandra from "../assets/sectiune/echipa/DragomirAlexandra.jpg";
+import GutuStefania from "../assets/sectiune/echipa/GutuStefania.jpeg";
+import IosifMaria from "../assets/sectiune/echipa/IosifMaria.jpg";
+import lazaroiuTeodora from "../assets/sectiune/echipa/lazaroiuTeodora.jpg";
+import mihaitaRazvan from "../assets/sectiune/echipa/mihaitaRazvan.jpg";
+import NicoiAlexandru from "../assets/sectiune/echipa/NicoiAlexandru.jpg";
+import NimaraDan from "../assets/sectiune/echipa/NimaraDan.jpg";
+import PopescuAlina from "../assets/sectiune/echipa/PopescuAlina.jpg";
+import StanaTheodora from "../assets/sectiune/echipa/StanaTheodora.jpg";
+import StanaVlad from "../assets/sectiune/echipa/StanaVlad.jpg";
+import DumitracheFlorentin from "../assets/sectiune/echipa/DumitracheFlorentin.jpeg";
+import DawodPaul from "../assets/sectiune/echipa/DawodPaul.jpg";
+
 // Import Animations
 import { motion } from "framer-motion";
 import {
@@ -100,12 +119,95 @@ const SectionPage = () => {
                   }
                 })}
 
-              <div></div>
+              {content && content.id === 6 && (
+                <div>
+                  <div className="paulImage"></div>
+                  <div className="textPaul">
+                    <p>
+                      Președintele Asociației Studenților la Matematică și
+                      Informatică
+                    </p>
+                    <p>Dawod Paul-Nasser</p>
+                  </div>
+                </div>
+              )}
+
+              {content && content.id === 15 && (
+                <motion.div className="cardImages">
+                  <div className="image">
+                    <img src={apostuAlexandru} alt="Apostu Alexandru" />
+                    <p className="imageText">Apostu Alexandru</p>
+                  </div>
+                  <div className="image">
+                    <img
+                      src={ApostuMihaiAdrian}
+                      alt="Apostu Mihai Adrian Image"
+                    />
+                    <p className="imageText">Apostu Mihai Adrian</p>
+                  </div>
+                  <div className="image">
+                    <img src={BadescuMadalina} alt="Bădescu Mădălina" />
+                    <p className="imageText">Bădescu Mădălina</p>
+                  </div>
+                  <div className="image">
+                    <img src={BuzamatIonut} alt="Buzamat Ionuț" />
+                    <p className="imageText">Buzamat Ionuț</p>
+                  </div>
+                  <div className="image">
+                    <img src={CordunDiana} alt="Cordun Diana" />
+                    <p className="imageText">Cordun Diana</p>
+                  </div>
+                  <div className="image">
+                    <img src={DragomirAlexandra} alt="Dragomir Alexandra" />
+                    <p className="imageText">Dragomir Alexandra</p>
+                  </div>
+                  <div className="image">
+                    <img src={DumitracheFlorentin} alt="DumitracheFlorentin" />
+                    <p className="imageText">Dumitrache Florentin</p>
+                  </div>
+                  <div className="image">
+                    <img src={GutuStefania} alt="Guțu Ștefania" />
+                    <p className="imageText">Guțu Ștefania</p>
+                  </div>
+                  <div className="image">
+                    <img src={IosifMaria} alt="Iosif Maria" />
+                    <p className="imageText">Iosif Maria</p>
+                  </div>
+                  <div className="image">
+                    <img src={lazaroiuTeodora} alt="Lăzăroiu Teodora" />
+                    <p className="imageText">Lăzăroiu Teodora</p>
+                  </div>
+                  <div className="image">
+                    <img src={mihaitaRazvan} alt="Mihăiță Răzvan" />
+                    <p className="imageText">Mihăiță Răzvan</p>
+                  </div>
+                  <div className="image">
+                    <img src={NicoiAlexandru} alt="Nicoi Alexandru" />
+                    <p className="imageText">Nicoi Alexandru</p>
+                  </div>
+                  <div className="image">
+                    <img src={NimaraDan} alt="Nimară Dan" />
+                    <p className="imageText">Nimară Dan</p>
+                  </div>
+                  <div className="image">
+                    <img src={PopescuAlina} alt="Popescu Alina" />
+                    <p className="imageText">Popescu Alina</p>
+                  </div>
+                  <div className="image">
+                    <img src={StanaTheodora} alt="Stană Theodora" />
+                    <p className="imageText">Stană Theodora</p>
+                  </div>
+                  <div className="image">
+                    <img src={StanaVlad} alt="Stană Vlad" />
+                    <p className="imageText">Stană Vlad</p>
+                  </div>
+                </motion.div>
+              )}
             </div>
           </motion.section>
         </motion.div>
       </motion.main>
-      <FooterComponent />
+      {/* <FooterComponent /> */}
     </>
   );
 };
